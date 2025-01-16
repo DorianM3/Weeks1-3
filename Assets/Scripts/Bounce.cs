51c6a94487ce11b0075fd1ed418ddbbd63af9121
Dorian Muhametaj <muhameta@shernet.sheridancollege.ca>
2025-01-16 15:11:12 -0500

    Made an object that bounces off the top and bottom walls at a random speed

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -9,30 +9,48 @@ public class Bounce : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      float speedX = Random.Range(-1.0f, 1.0f);
-      float speedY = Random.Range(-1.0f, 1.0f);
+       speedX = Random.Range(-5.0f, 5.0f);
+       speedY = Random.Range(-5.0f, 5.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
         Vector3 pos = transform.position;
-
-        if (pos.x < 0 || pos.x > Screen.width)
-        {
-            speedX *= -0.1f;
-        }
-
-        if(pos.y < 0 || pos.y > Screen.height)
-        {
-            speedY *= -0.1f; 
-        }
-
         pos.x += speedX * Time.deltaTime;
         pos.y += speedY * Time.deltaTime;
 
-        transform.position += pos; 
-       */ 
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+
+        if (screenPos.x < 0)
+        {
+            Vector3 fixedPos = new Vector3(0,0,0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+            speedX *= -1f;
+        }
+
+        if(screenPos.x > Screen.width)
+        {
+            Vector3 fixedPos = new Vector3(Screen.width, 0, 0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+            speedX *= -1f;
+        }
+
+        if (screenPos.y < 0)
+        {
+            Vector3 fixedPos = new Vector3(0, 0, 0);
+            pos.y = Camera.main.ScreenToWorldPoint(fixedPos).y;
+            speedY *= -1f;
+        }
+
+        if (screenPos.y > Screen.height)
+        {
+            Vector3 fixedPos = new Vector3(0, Screen.height, 0);
+            pos.y = Camera.main.ScreenToWorldPoint(fixedPos).y;
+            speedY *= -1f;
+        }
+
+        transform.position = pos; 
+
     }
 }
