7166e12461a031eaa297f1bc919b8d3e919ce3bd
Dorian Muhametaj <muhameta@shernet.sheridancollege.ca>
2025-01-26 10:34:32 -0500

    Made object patrol from a start and end point without the need for human input touching the t slider

--- Assets/Scripts/LerpPatrol.cs
+++ Assets/Scripts/LerpPatrol.cs
@@ -7,6 +7,7 @@ public class LerpPatrol : MonoBehaviour
     public AnimationCurve curve;
     [Range(0, 1)]
     public float t;
+    public float tminus; 
     public Transform start;
     public Transform end;
 
@@ -14,12 +15,22 @@ public class LerpPatrol : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //Tminus is so we can add to t automatically without needing to adjust the slider 
+        tminus = -0.1f; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //This line is what allows for the translation between two empty gameobjects 
         transform.position = Vector2.Lerp(start.position, end.position, t);
+        //If our object hits either side of the screen the direction flips
+        if(t <= 0 || t >= 1)
+        {
+            tminus *= -1; 
+        }
+
+        //Adds or subtracts to t and uses time.deltatime to assure the pace is steady 
+        t += tminus * Time.deltaTime;
     }
 }
